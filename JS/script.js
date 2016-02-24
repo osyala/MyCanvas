41b366921457c1b0d6812e9b75e3781ce53123df
Oscar Ayala <osyala@gmail.com>
2016-02-24 17:23:41 -0600

    adding styling

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,12 @@
-// Getting instafeed ready to deploy pictures to my "instafeed" div.
-var Instafeed = require("instafeed.js");
 
-
-var feed = new Instafeed ({
-	get: "tagged",
-	tagName: "yeezyforsale",
-	clientId: "ef2697a072c24e968b67f86c7850f95c"
-});
+var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'yeezyforsale',
+    limit: "32",
+    // resolution: 'low_resolution',
+        // userId: '26620985',
+    accessToken: '26620985.745c638.74a59aa021a84eeab1d50b4c7c687180'
+        // clientId: 'ef2697a072c24e968b67f86c7850f95c'
+    });
 
 feed.run();
